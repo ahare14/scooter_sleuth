ccb02c852b0769fcff25f243fc46fd1df9975f81
ahare14 <alexanderhare14@gmail.com>
2019-08-13 10:36:18 -0600

    updated display

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//   const endPoint = 'http://localhost:3000/api/v1/users';
-//   fetch(endPoint)
-//     .then(res => res.json())
-//     .then(json => console.log(json));
-// });
 const spinUrl = 'https://web.spin.pm/api/gbfs/v1/denver/free_bike_status'
 const birdUrl = 'https://api.bird.co/bird/nearby?latitude=39.766271&longitude=-104.951056&radius=1000'
 const lyftUrl = ''
@@ -62,9 +56,7 @@ function getBird() {
     })
   }
 
-
-
-// grabs map location
+// load window with map and time
 window.addEventListener('load', loadMap)
 
 function loadMap() {
@@ -76,9 +68,16 @@ function loadMap() {
   })
 }
 
+// dynamic time rendering
 function startTime() {
   let today = new Date();
-  let h = today.getHours() - 12;
+  let h
+  if (today.getHours() <= 12) {
+    h = today.getHours()
+  } else {
+    h = today.getHours() - 12
+  }
+  // let h = today.getHours();
   let m = today.getMinutes();
   let s = today.getSeconds();
   m = checkTime(m);
@@ -99,7 +98,7 @@ let geocoder
 function initMap(latitude,longitude) {
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: latitude, lng: longitude},
-    zoom: 12
+    zoom: 16
   })
   geocoder = new google.maps.Geocoder;
 }
@@ -287,203 +286,4 @@ function removeJumpBikes() {
   })
 }
 
-// dropdown.addEventListener('click', userLogin)
-// const h1Welcome = document.querySelector('#welcome')
-// let humanLocator = {}
-// function userLogin (event) {
-//   event.preventDefault()
-//   const person = event.target.textContent
-//   console.log(perons)
-//   createHumanObj(person)
-//   h1Welcome.textContent = `Welcome ${event.target.textContent.split(' ')[0]} Lets Sleuth!`
-// }
 
-// function loadContent() {
-//   const x = document.querySelector('#firstHeading')
-//   console.log(humanLocator)
-// }
-
-// function createHumanObj (person) {
-//   users.forEach(function(individual){
-//     if (person === `${individual.first_name} ${individual.last_name}`) {
-//       humanLocator[individual.id] = `${individual.first_name} ${individual.last_name}`
-//     } else {
-//       alert('error you idiot')
-//     }
-//   })
-// }
-
-const spinScooters = [
-      {
-        "bike_id": "f1e6cdb5-bb2f-4cfe-9225-860a75436210",
-        "lat": 39.76307,
-        "lon": -105.00112,
-        "vehicle_type": "scooter",
-        "is_reserved": 0,
-        "is_disabled": 0
-      },
-      {
-        "bike_id": "c382b74d-61db-4025-9385-a5000bb5087f",
-        "lat": 39.76288,
-        "lon": -104.98838,
-        "vehicle_type": "scooter",
-        "is_reserved": 0,
-        "is_disabled": 0
-      },
-      {
-        "bike_id": "3860ce10-a0cd-4d4f-929b-1d35a2ef0a3e",
-        "lat": 39.75495,
-        "lon": -105.00187,
-        "vehicle_type": "scooter",
-        "is_reserved": 0,
-        "is_disabled": 0
-      },
-      {
-        "bike_id": "761c6a7e-095a-4360-af0c-0d8c4572c694",
-        "lat": 39.75355,
-        "lon": -104.99216,
-        "vehicle_type": "scooter",
-        "is_reserved": 0,
-        "is_disabled": 0
-      },
-      {
-        "bike_id": "52af5537-7a5b-4232-ae84-70877d6484d7",
-        "lat": 39.75268,
-        "lon": -104.99936,
-        "vehicle_type": "scooter",
-        "is_reserved": 0,
-        "is_disabled": 0
-      }]
-
-// const jump = [{
-//         "bike_id": "bike_90722",
-//         "name": "19401",
-//         "lon": -104.986985,
-//         "lat": 39.75803833333333,
-//         "is_reserved": 0,
-//         "is_disabled": 0,
-//         "jump_ebike_battery_level": "48%",
-//         "jump_vehicle_type": "bike"
-//       },
-//       {
-//         "bike_id": "bike_184853",
-//         "name": "32814",
-//         "lon": -104.990265,
-//         "lat": 39.74614666666667,
-//         "is_reserved": 0,
-//         "is_disabled": 0,
-//         "jump_ebike_battery_level": "35%",
-//         "jump_vehicle_type": "bike"
-//       },
-//       {
-//         "bike_id": "bike_90709",
-//         "name": "19403",
-//         "lon": -105.011435,
-//         "lat": 39.78386833333333,
-//         "is_reserved": 0,
-//         "is_disabled": 0,
-//         "jump_ebike_battery_level": "83%",
-//         "jump_vehicle_type": "bike"
-//       },
-//       {
-//         "bike_id": "bike_90628",
-//         "name": "18923",
-//         "lon": -104.99449333333334,
-//         "lat": 39.743338333333334,
-//         "is_reserved": 0,
-//         "is_disabled": 0,
-//         "jump_ebike_battery_level": "63%",
-//         "jump_vehicle_type": "bike"
-//       },
-//       {
-//         "bike_id": "bike_184401",
-//         "name": "08682",
-//         "lon": -104.97072333333334,
-//         "lat": 39.745691666666666,
-//         "is_reserved": 0,
-//         "is_disabled": 0,
-//         "jump_ebike_battery_level": "31%",
-//         "jump_vehicle_type": "bike"
-//       },
-//       {
-//         "bike_id": "bike_90657",
-//         "name": "19429",
-//         "lon": -104.99039333333333,
-//         "lat": 39.74109166666667,
-//         "is_reserved": 0,
-//         "is_disabled": 0,
-//         "jump_ebike_battery_level": "83%",
-//         "jump_vehicle_type": "bike"
-//       }]
-
-const birdData = [
-      {
-          "id": "a28f062e-f6b7-40c7-9ca9-b834993bd5c4",
-          "location": {
-              "latitude": 39.754489,
-              "longitude": -104.950055
-          },
-          "code": "",
-          "captive": false,
-          "battery_level": 78
-      },
-      {
-          "id": "5b21630d-40e3-46ec-b562-0015b1955ef3",
-          "location": {
-              "latitude": 39.751144,
-              "longitude": -104.948703
-          },
-          "code": "",
-          "captive": false,
-          "battery_level": 92
-      },
-      {
-          "id": "876f67e5-441a-4282-92c7-585abf706c83",
-          "location": {
-              "latitude": 39.751058,
-              "longitude": -104.948676
-          },
-          "code": "",
-          "captive": false,
-          "battery_level": 98
-      },
-      {
-          "id": "d5d4d170-30c4-47e1-bccb-f6b912e9358b",
-          "location": {
-              "latitude": 39.770133,
-              "longitude": -104.971596
-          },
-          "code": "",
-          "captive": false,
-          "battery_level": 52
-      },
-      {
-          "id": "0f4518c6-37a1-444b-b5b6-a55a033fdd1e",
-          "location": {
-              "latitude": 39.766238,
-              "longitude": -104.972948
-          },
-          "code": "",
-          "captive": false,
-          "battery_level": 27
-      },
-      {
-          "id": "89715907-801b-4bd6-b89f-4dfe9dbebd43",
-          "location": {
-              "latitude": 39.766457,
-              "longitude": -104.973205
-          },
-          "code": "",
-          "captive": false,
-          "battery_level": 55
-      },
-      {
-          "id": "2ad760b8-a852-41fd-948d-7ae0e61d0486",
-          "location": {
-              "latitude": 39.766286,
-              "longitude": -104.973262
-          },
-          "code": "",
-          "captive": false,
-          "battery_level": 29
-      }]
---
+++ dummyData.js
@@ -0,0 +1,174 @@
+const spinScooters = [
+      {
+        "bike_id": "f1e6cdb5-bb2f-4cfe-9225-860a75436210",
+        "lat": 39.76307,
+        "lon": -105.00112,
+        "vehicle_type": "scooter",
+        "is_reserved": 0,
+        "is_disabled": 0
+      },
+      {
+        "bike_id": "c382b74d-61db-4025-9385-a5000bb5087f",
+        "lat": 39.76288,
+        "lon": -104.98838,
+        "vehicle_type": "scooter",
+        "is_reserved": 0,
+        "is_disabled": 0
+      },
+      {
+        "bike_id": "3860ce10-a0cd-4d4f-929b-1d35a2ef0a3e",
+        "lat": 39.75495,
+        "lon": -105.00187,
+        "vehicle_type": "scooter",
+        "is_reserved": 0,
+        "is_disabled": 0
+      },
+      {
+        "bike_id": "761c6a7e-095a-4360-af0c-0d8c4572c694",
+        "lat": 39.75355,
+        "lon": -104.99216,
+        "vehicle_type": "scooter",
+        "is_reserved": 0,
+        "is_disabled": 0
+      },
+      {
+        "bike_id": "52af5537-7a5b-4232-ae84-70877d6484d7",
+        "lat": 39.75268,
+        "lon": -104.99936,
+        "vehicle_type": "scooter",
+        "is_reserved": 0,
+        "is_disabled": 0
+      }]
+
+const jump = [{
+        "bike_id": "bike_90722",
+        "name": "19401",
+        "lon": -104.986985,
+        "lat": 39.75803833333333,
+        "is_reserved": 0,
+        "is_disabled": 0,
+        "jump_ebike_battery_level": "48%",
+        "jump_vehicle_type": "bike"
+      },
+      {
+        "bike_id": "bike_184853",
+        "name": "32814",
+        "lon": -104.990265,
+        "lat": 39.74614666666667,
+        "is_reserved": 0,
+        "is_disabled": 0,
+        "jump_ebike_battery_level": "35%",
+        "jump_vehicle_type": "bike"
+      },
+      {
+        "bike_id": "bike_90709",
+        "name": "19403",
+        "lon": -105.011435,
+        "lat": 39.78386833333333,
+        "is_reserved": 0,
+        "is_disabled": 0,
+        "jump_ebike_battery_level": "83%",
+        "jump_vehicle_type": "bike"
+      },
+      {
+        "bike_id": "bike_90628",
+        "name": "18923",
+        "lon": -104.99449333333334,
+        "lat": 39.743338333333334,
+        "is_reserved": 0,
+        "is_disabled": 0,
+        "jump_ebike_battery_level": "63%",
+        "jump_vehicle_type": "bike"
+      },
+      {
+        "bike_id": "bike_184401",
+        "name": "08682",
+        "lon": -104.97072333333334,
+        "lat": 39.745691666666666,
+        "is_reserved": 0,
+        "is_disabled": 0,
+        "jump_ebike_battery_level": "31%",
+        "jump_vehicle_type": "bike"
+      },
+      {
+        "bike_id": "bike_90657",
+        "name": "19429",
+        "lon": -104.99039333333333,
+        "lat": 39.74109166666667,
+        "is_reserved": 0,
+        "is_disabled": 0,
+        "jump_ebike_battery_level": "83%",
+        "jump_vehicle_type": "bike"
+      }]
+
+const birdData = [
+      {
+          "id": "a28f062e-f6b7-40c7-9ca9-b834993bd5c4",
+          "location": {
+              "latitude": 39.754489,
+              "longitude": -104.950055
+          },
+          "code": "",
+          "captive": false,
+          "battery_level": 78
+      },
+      {
+          "id": "5b21630d-40e3-46ec-b562-0015b1955ef3",
+          "location": {
+              "latitude": 39.751144,
+              "longitude": -104.948703
+          },
+          "code": "",
+          "captive": false,
+          "battery_level": 92
+      },
+      {
+          "id": "876f67e5-441a-4282-92c7-585abf706c83",
+          "location": {
+              "latitude": 39.751058,
+              "longitude": -104.948676
+          },
+          "code": "",
+          "captive": false,
+          "battery_level": 98
+      },
+      {
+          "id": "d5d4d170-30c4-47e1-bccb-f6b912e9358b",
+          "location": {
+              "latitude": 39.770133,
+              "longitude": -104.971596
+          },
+          "code": "",
+          "captive": false,
+          "battery_level": 52
+      },
+      {
+          "id": "0f4518c6-37a1-444b-b5b6-a55a033fdd1e",
+          "location": {
+              "latitude": 39.766238,
+              "longitude": -104.972948
+          },
+          "code": "",
+          "captive": false,
+          "battery_level": 27
+      },
+      {
+          "id": "89715907-801b-4bd6-b89f-4dfe9dbebd43",
+          "location": {
+              "latitude": 39.766457,
+              "longitude": -104.973205
+          },
+          "code": "",
+          "captive": false,
+          "battery_level": 55
+      },
+      {
+          "id": "2ad760b8-a852-41fd-948d-7ae0e61d0486",
+          "location": {
+              "latitude": 39.766286,
+              "longitude": -104.973262
+          },
+          "code": "",
+          "captive": false,
+          "battery_level": 29
+      }]
